1ce54ceebb504aeccd5cf94dbb376cb17f53d198
Ahmed Nasr <ahmed.harg@yahoo.com>
2020-03-18 22:05:33 +0200

    manifest_update

--- TripRemainderProject/app/src/main/java/com/example/trip_remainder_project/Views/HomeView/MainActivity.java
+++ TripRemainderProject/app/src/main/java/com/example/trip_remainder_project/Views/HomeView/MainActivity.java
@@ -13,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // In the name of Allah
+        // testing line
+        int x=90;
     }
 }
