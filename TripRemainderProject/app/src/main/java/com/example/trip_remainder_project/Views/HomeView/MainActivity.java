c1f049cfa6a7cae2f06a1fa159480dc3f20224b6
Ahmed Nasr <ahmed.harg@yahoo.com>
2020-03-19 02:59:27 +0200

    nav-drawer

--- TripRemainderProject/app/src/main/java/com/example/trip_remainder_project/Views/HomeView/MainActivity.java
+++ TripRemainderProject/app/src/main/java/com/example/trip_remainder_project/Views/HomeView/MainActivity.java
@@ -1,19 +1,39 @@
 package com.example.trip_remainder_project.Views.HomeView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.trip_remainder_project.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout=findViewById(R.id.drawer);
+        actionBarDrawerToggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         // In the name of Allah
         // testing line
         int x=90;
+        int y=90;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
