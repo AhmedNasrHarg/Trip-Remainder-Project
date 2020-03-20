dc505f47c49b724bd2be69b20e3d28ca5cc596be
ayahamdy97 <ayahamdy692@gmail.com>
2020-03-20 12:14:49 +0200

    trying github

--- TripRemainderProject/app/src/main/java/com/example/trip_remainder_project/Views/TripView/TripActivity.java
+++ TripRemainderProject/app/src/main/java/com/example/trip_remainder_project/Views/TripView/TripActivity.java
@@ -13,5 +13,7 @@ public class TripActivity extends AppCompatActivity implements TripContract.IVie
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip);
+
+        //i was here 
     }
 }
