9376b65c304ad94a05549cb6a5abe80aee4787e9
Marina-Sameh95 <58281844+Marina-Sameh95@users.noreply.github.com>
2020-03-20 19:28:39 +0200

    add login

--- TripRemainderProject/app/src/main/java/com/example/trip_remainder_project/Views/Login/Login.java
+++ TripRemainderProject/app/src/main/java/com/example/trip_remainder_project/Views/Login/Login.java
@@ -3,14 +3,33 @@ package com.example.trip_remainder_project.Views.Login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.trip_remainder_project.R;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
+    EditText emailTxt;
+    EditText passTxt;
+    Button btnLogin;
+    Button btnReg;
+    Button btnGoogle;
+    FirebaseAuth firebaseAuth;
+    GoogleSignInClient mGoogleSignInClient ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailTxt = findViewById(R.id.emailTxt);
+        passTxt = findViewById(R.id.passTxt);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnReg = findViewById(R.id.btnReg);
+        btnGoogle = findViewById(R.id.btnGoogle);
+        firebaseAuth = FirebaseAuth.getInstance();
+
     }
 }
