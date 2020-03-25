60b687a6328c18a765744cb9d1b7a4c5e5493b38
Ahmed Nasr <ahmed.harg@yahoo.com>
2020-03-25 02:09:08 +0200

    trip-details

--- TripPlanner/app/src/main/java/com/example/tripplanner/Views/TripDetails/TripDetails.java
+++ TripPlanner/app/src/main/java/com/example/tripplanner/Views/TripDetails/TripDetails.java
@@ -9,6 +9,8 @@ import android.widget.TextView;
 import com.example.tripplanner.POJOs.Trip;
 import com.example.tripplanner.R;
 
+import java.util.ArrayList;
+
 public class TripDetails extends AppCompatActivity {
 
     @Override
@@ -24,6 +26,7 @@ public class TripDetails extends AppCompatActivity {
         TextView date=findViewById(R.id.tripDetailDate);
         TextView time=findViewById(R.id.tripDetailTime);
         TextView type=findViewById(R.id.tripDetailType);
+        TextView notes=findViewById(R.id.tripDetailNotes);
 
         status.setText(curTrip.getStatus());
         name.setText(curTrip.getTripName());
@@ -32,5 +35,11 @@ public class TripDetails extends AppCompatActivity {
         date.setText(curTrip.getTripDate());
         time.setText(curTrip.getTripTime());
         type.setText(curTrip.getTripType());
+        String allNotes="";
+        ArrayList<String>li=curTrip.getNotes();
+        for(int i=0;i<li.size();i++){
+            allNotes+=(li.get(i)+", ");
+        }
+        notes.setText(allNotes);
     }
 }
