d49713e5ccbafacebd532133486e60ab45d06cd9
Ahmed Nasr <ahmed.harg@yahoo.com>
2020-04-01 19:35:07 +0200

    start-fixt

--- TripPlanner/app/src/main/java/com/example/tripplanner/Views/TripView/Dialog.java
+++ TripPlanner/app/src/main/java/com/example/tripplanner/Views/TripView/Dialog.java
@@ -1,17 +1,14 @@
 package com.example.tripplanner.Views.TripView;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.tripplanner.Models.DialogModel.DialogContract;
-import com.example.tripplanner.POJOs.Trip;
 import com.example.tripplanner.Presenters.DialogPresenter.DialogPresenter;
 import com.example.tripplanner.R;
 
@@ -41,11 +38,8 @@ public class Dialog extends AppCompatActivity implements DialogContract.IView {
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // send trip to Foreground using intent to update reqCode
-                Intent intent=new Intent(v.getContext(), ForegroundService.class);
-                intent.putExtra("reqCode",reqCode);
-                startService(intent);
                 openMap();
+                Dialog.this.finish();
                 presenter.handleDoneTrip(id);
             }
         });
